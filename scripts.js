2cbb8665f2a50fdfe18320963d781cc5ce72d025
Chris Underwood <cunderwoodmn@gmail.com>
2017-12-25 18:53:45 -0800

    add code to allow clicking on the progress bar to update the video to that time

--- scripts.js
+++ scripts.js
@@ -35,7 +35,10 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`;
 }
 
-
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
 
 /* hook event listeners */
 video.addEventListener('click', togglePlay);
@@ -47,3 +50,5 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
